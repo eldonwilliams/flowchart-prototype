b958961bf62d60a270dfb2f9b92e9d6978377467
Eldon Williams <eldon.ew.com@gmail.com>
2023-11-10 16:45:30 -0600

    split up behavior into components

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { useState } from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import FlowchartRender from './components/FlowchartRender';
-import { FlowchartSelectionState } from './FlowchartComponents';
 
 function App() {
-  const [selection, setSelection] = useState<FlowchartSelectionState>("pointer" as FlowchartSelectionState);
-
   return (
     <div id="app">
-      <Sidebar
-        onSidebarClicked={(e) => setSelection(e.selection)}
-      />
-      <FlowchartRender selectionState={selection} />
+      <Sidebar />
+      <FlowchartRender />
     </div>
   )
 }
