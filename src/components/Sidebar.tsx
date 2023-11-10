150c7d54a09cf8646f0d3927d8d51300b175b056
Eldon Williams <eldon.ew.com@gmail.com>
2023-11-10 16:46:02 -0600

    simplify sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,29 +1,16 @@
 import { ComponentPropsWithoutRef } from 'react';
 import './Sidebar.css';
-import { FlowchartSelectionState } from '../FlowchartComponents';
-
-export interface SidebarClickEvent extends MouseEvent {
-    selection: FlowchartSelectionState;
-}
 
 export interface SideBarProps extends ComponentPropsWithoutRef<"div"> {
-    onSidebarClicked: (e: SidebarClickEvent) => void; 
+
 }
 
-export default function Sidebar(_props: SideBarProps) {
-    const { onSidebarClicked, ...props } = _props;
-    
-    function handleClick(e: MouseEvent, selection: string) {
-        const sidebarClickEvent: SidebarClickEvent = e as SidebarClickEvent;
-        sidebarClickEvent.selection = selection as FlowchartSelectionState;
-        onSidebarClicked(sidebarClickEvent);
-    }
-
+export default function Sidebar(props: SideBarProps) {
     return (
         <div {...props} id="sidebar" >
-            <div onClick={(e: any) => handleClick(e, "box")}>Box</div>
-            <div onClick={(e: any) => handleClick(e, "line")}>Line</div>
-            <div onClick={(e: any) => handleClick(e, "pointer")}>Pointer</div>
+            <div>Box</div>
+            <div>Line</div>
+            <div>Pointer</div>
         </div>
     )
 }
