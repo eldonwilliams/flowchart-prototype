ee0ee7f9e08d21af845d9bd639b6007e745839b4
Eldon Williams <eldon.ew.com@gmail.com>
2023-11-13 10:24:47 -0600

    add uuid, helper function, and uuid

--- src/FlowchartComponents/index.ts
+++ src/FlowchartComponents/index.ts
@@ -4,10 +4,53 @@ export enum FlowchartComponentTypes {
 }
 
 export default interface FlowchartComponent {
+    /**
+     * A unique identifier for this component
+     */
+    uuid: string;
+
+    /**
+     * The x position of the component
+     */
     x: number;
+
+    /**
+     * the y position of the component
+     */
     y: number;
+
+    /**
+     * The width of the compoennt
+     */
     width: number;
+
+    /**
+     * The height of the component
+     */
     height: number;
+
+    /**
+     * The rotation of the component
+     */
+    rotation: number;
+
+    /**
+     * The connections to other flowchart components
+     * @todo currently does nothing
+     */
     connections: FlowchartComponent[],
+
+    /**
+     * The type of component this is
+     */
     type: FlowchartComponentTypes,
+}
+
+export function createFlowchartComponent(type: FlowchartComponentTypes, x: number = 0, y: number = 0,
+    height: number = 10, width: number = 10, rotation: number = 0): FlowchartComponent {
+        return ({
+            x, y, height, width, rotation, type,
+            connections: [],
+            uuid: crypto.randomUUID(),
+        })
 }
