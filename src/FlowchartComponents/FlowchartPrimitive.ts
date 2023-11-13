bf86daa22ec7d0bbc610058211e95b5f15fdaebe
Eldon Williams <eldon.ew.com@gmail.com>
2023-11-12 19:03:52 -0600

    moving to a data based solution

---
+++ src/FlowchartComponents/FlowchartPrimitive.ts
@@ -0,0 +1,9 @@
+/**
+ * A type that represents data that is required for any Flowchart component
+ */
+export default interface FlowchartPrimitive {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+}
